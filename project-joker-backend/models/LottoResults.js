2f2d3b9099779c31664215e01782343e616940a8
Milan Sułek <sulekmilan00@gmail.com>
2025-03-05 18:46:56 +0100

    add backend for application

---
+++ project-joker-backend/models/LottoResults.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const lottoResultsSchema = new mongoose.Schema({
+  SelectedNumbers: {
+    type: [Number], // Tablica liczb
+    required: true,
+  },
+  WiningNumbers: {
+    type: [Number], // Tablica liczb
+    required: true,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+  IP: {
+    type: String,
+    required: true,
+  },
+  MatchCount: {
+    type: Number,
+    required: true,
+  },
+  PrizePool: {
+    type: Number,
+    default: 0,
+  },
+  DidWin: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const LottoResults = mongoose.model("LottoResults", lottoResultsSchema);
+
+export default LottoResults;
