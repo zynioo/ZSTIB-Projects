a7a87c47b21c5cc7fd5ce9386265cd99ae9fcffa
Michal Zach <michalzach69@gmail.com>
2025-03-05 22:31:14 +0100

    repair bug in fetching from backend

--- project-joker/src/views/LottoGameView/LottoGameResultPanel.vue
+++ project-joker/src/views/LottoGameView/LottoGameResultPanel.vue
@@ -60,7 +60,7 @@ const emitReturnToTable = () => {
 const fetchResults = async () => {
   try {
     const response = await fetch(
-      "http://localhost:5000/api/generateLottoNumbers",
+      "/api/generateLottoNumbers",
       {
         method: "POST",
         headers: {
